0a6865731aecbbac4a1569b9fe2dcf0e2e770ed1
Reshan Gayantha <reshangayantha9@gmail.com>
2024-08-15 10:01:51 +0530

    Initializer

---
+++ src/main/java/com/example/audibook/AudibookApplication.java
@@ -0,0 +1,13 @@
+package com.example.audibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudibookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/audibook/AudibookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
